c9b02cfb441d6006bad9f5b5aa944abbea1ed354
Omar <48713070+Omar-Belghaouti@users.noreply.github.com>
2019-04-23 19:53:04 +0200

    Update Lorenz.m

--- Lorenz.m
+++ Lorenz.m
@@ -1,15 +1,15 @@
 clc, clear, close all;
-% Initialisation de vecteur des états initiales :
+% Initial states vector :
 x0 = [.1 .1 .1];
-% Appel de la fonction ode45 :
+% Calling function ode45 :
 [T, X] = ode45('fct5', [0 100], x0);
-% Représentation des trajectoires temporelles :
+% Representation of temporal trajectories :
 plot(T, X(:,1), 'r-', ...
     T, X(:,2), 'b-', ...
     T, X(:,3), 'g-');
 grid on;
 legend('x', 'y', 'z');
-% Représentation du portrait de phase :
+% Representation of the phase portrait :
 figure;
 plot3(X(:,1), X(:,2), X(:,3));
 grid on;
